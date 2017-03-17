36a785e6a7a2ba8bb7ed6c20d0d460cc012ede6e
邵博超DEXION <dexter0ion@gmail.com>
2017-03-17 21:36:17 +0800

    3/17

---
+++ MovingObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+
+	// Use this for initialization
+	protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
